f43bfba0e5a48a1a2f5f78f6fbe33e2e8d469bc9
Noah Barney <nbarney@rsgc.on.ca>
2016-02-01 10:30:52 -0500

    work from class

--- Tick Challenges/Group-Maker.playground/Contents.swift
+++ Tick Challenges/Group-Maker.playground/Contents.swift
@@ -1,6 +1,10 @@
 //: Playground - noun: a place where people can play
 
 import Cocoa
+//1. create number of groups from the list given
+//2. groups should be random
+
+
 
 //Matthew Anderson
 //Stuart Armstrong
@@ -27,4 +31,63 @@ import Cocoa
 //Matthew Pritchard
 //Sean Trevor
 
-var str = "Hello, playground"
+// Create data structure
+
+var studentList = [String]()
+
+studentList.append ("Matthew Anderson")
+studentList.append ("Stuart Armstrong")
+studentList.append ("Puneet Bagga")
+studentList.append ("Noah Barney")
+studentList.append ("Scott Blackwell")
+studentList.append ("Mattias Blum")
+studentList.append ("Jeremy Burleton")
+studentList.append ("Liam Byrne")
+studentList.append ("Gilbert Chien")
+studentList.append ("Thomas Davies")
+studentList.append ("Maxym Dubczak")
+studentList.append ("Andrew Elder")
+studentList.append ("Oliver Gane")
+studentList.append ("Jeffrey Goldsmith")
+studentList.append ("William Graham")
+studentList.append ("Michael Green")
+studentList.append ("Nicholas Jones")
+studentList.append ("Brendan Leder")
+studentList.append ("Alex Lo")
+studentList.append ("Hudson McDaniel")
+studentList.append ("Mark McCutcheon")
+studentList.append ("Robert Miller")
+studentList.append ("Carlos Noble Curveira")
+studentList.append ("Matthew Pritchard")
+studentList.append ("Sean Trevor")
+
+var studentRandomPosition = [Int] ()
+for i in 1...25  {
+    print ("\(i)")
+    studentRandomPosition.append(Int(arc4random_uniform(1000)))
+}
+
+//Find highest number in list of random numbers
+//for i in 1...25 {
+//    print(" The current value is: \(studentRandomPosition [i])")
+//    
+//}
+
+//ABOVE: an example of using a "regular" loop to iterate over an array.
+// we've illustrated a common run-time error- out of bounds condition. 
+// we ran over the end of the array
+for (index, value)
+    in studentRandomPosition.enumerate(){
+        print ("The index is\(index) and the value is\(value)")
+    
+}
+
+
+
+
+
+
+
+
+
+
